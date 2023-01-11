7209c3793e1239e7ccd93ba68676332a7959075b
GabrielePagnotta <gabriele.pagnotta1998@gmail.com>
2023-01-11 20:45:03 +0100

    some little changes about the nav

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,3 +1,14 @@
+<?php 
+$name= "Gabriele";
+$world="Hello World";
+
+$arraynav = [
+    ["href"=>"http://127.0.0.1:8000/homepage/page1","text"=>"About"],
+    ["href"=>"http://127.0.0.1:8000/homepage/page2","text"=>"Chi Sono"]
+];
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,8 +20,60 @@
     <title>Document</title>
 </head>
 <body>
-    <h1 class="bg-warning">HELLO WORLD</h1>
-    <a href="http://127.0.0.1:8000/homepage/page1">page1</a>
-    <a href="http://127.0.0.1:8000/homepage/page2">page2</a>
+    <header class="container">
+        <div>
+            <img class="logo" src="https://laravel.com/img/logomark.min.svg" alt="laravel-logo">
+            <img src="https://laravel.com/img/logotype.min.svg" alt="laravel-logo-text">
+            
+        </div>
+        
+        <div>
+            <?php foreach ($arraynav as $key) {?>
+                 <a href="<?php echo $key["href"] ?>"><?php echo $key["text"] ?></a>
+            <?php } ?>
+
+        </div>
+    
+    </header>
+    <body>
+        <h1 class="display">{{$world}}</h1>    
+    </body>
+
+    
+    
 </body>
+<style>
+
+    *{
+        margin:0;
+        padding:0;
+        box-sizing:border-box;
+        text-decoration-line: none;
+    }
+    .container{
+       display:flex;
+       justify-content:space-between;
+       width: 100%;
+       margin:0 auto;
+       align-items:center;
+       background-color:#73a7a7;
+       padding:20px 100px;
+
+    }
+
+    .logo{
+        width:30%;
+        display:inline-block;
+    }
+
+    a{
+        margin:20px;
+        font-size:1.5rem;
+        color:black;   
+    }
+
+    body{
+        text-align:center;
+    }
+</style>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
 Route::get('/', function () {
+    
     return view('homepage');
 });
 Route::get('homepage/page1', function () {
